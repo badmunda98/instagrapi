38680082c5fabc10f10c98564bb874c4acef096e
Devesh Pal <competitive.schooling.info@gmail.com>
2021-10-04 16:17:11 +0530

    Update exceptions.py

--- instagrapi/exceptions.py
+++ instagrapi/exceptions.py
@@ -71,6 +71,10 @@ class PrivateError(ClientError):
     """For Private API and last_json logic"""
 
 
+class NotFoundError(PrivateError):
+    reason = 'Not found'
+
+
 class FeedbackRequired(PrivateError):
     pass
 
@@ -141,7 +145,7 @@ class MediaError(PrivateError):
     pass
 
 
-class MediaNotFound(MediaError):
+class MediaNotFound(NotFoundError, MediaError):
     pass
 
 
@@ -149,7 +153,7 @@ class UserError(PrivateError):
     pass
 
 
-class UserNotFound(UserError):
+class UserNotFound(NotFoundError, UserError):
     pass
 
 
@@ -157,23 +161,19 @@ class CollectionError(PrivateError):
     pass
 
 
-class CollectionNotFound(CollectionError):
-    def __init__(self, *args, **kwargs):
-        super().__init__(
-            f"Collection \"{kwargs.get('name')}\" not found",
-            *args, **kwargs
-        )
+class CollectionNotFound(NotFoundError, CollectionError):
+    pass
 
 
 class DirectError(PrivateError):
     pass
 
 
-class DirectThreadNotFound(DirectError):
+class DirectThreadNotFound(NotFoundError, DirectError):
     pass
 
 
-class DirectMessageNotFound(DirectError):
+class DirectMessageNotFound(NotFoundError, DirectError):
     pass
 
 
@@ -246,24 +246,21 @@ class HashtagError(PrivateError):
     pass
 
 
-class HashtagNotFound(HashtagError):
-    def __init__(self, *args, **kwargs):
-        super().__init__(
-            f"Hashtag \"{kwargs.get('name')}\" not found",
-            *args, **kwargs
-        )
+class HashtagNotFound(NotFoundError, HashtagError):
+    pass
 
 
 class LocationError(PrivateError):
     pass
 
 
-class LocationNotFound(LocationError):
-    def __init__(self, *args, **kwargs):
-        super().__init__(
-            f"Location \"{kwargs.get('location_pk')}\" not found",
-            *args, **kwargs
-        )
+class LocationNotFound(NotFoundError, LocationError):
+    pass
+
 
 class TwoFactorRequired(PrivateError):
+    pass
+
+
+class HighlightNotFound(NotFoundError, PrivateError):
     pass
